9f2b752428589c940601724587422ec909e29413
Alex-Bull-Tech <alex.co.uk@hotmail.com>
2022-12-05 11:55:56 +0000

    Changing hello word

--- app/main.py
+++ app/main.py
@@ -28,5 +28,5 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welocome to my API ! ! ! !"}
+    return {"message": "Artiom you are a FUCKER ! ! ! ! !"}
      
